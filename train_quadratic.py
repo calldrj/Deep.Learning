c46f5b7a62ba82876701cab7cd0f5b21d3b9c1b7
Tuan Nguyen <efranchisor@gmail.com>
2020-06-01 18:36:05 -0700

    add regulation factor to the training process

--- train_quadratic.py
+++ train_quadratic.py
@@ -1,13 +1,15 @@
 import sys
 sys.path.append('./data')
-import neuron_net
+import quadratic_network
 import data_loader
 # Load datasets
 training_data, validation_data, test_data = data_loader.wrap_data()
 # Create and configure a network
-net = neuron_net.DeepNet([784, 30, 10])
+net = quadratic_network.DeepNet([784, 30, 10])
 # Train the network with hyper-parameters
 num_epochs = 30
 batch_size = 10
-learning_rate = 1.0
-net.SGD(training_data, num_epochs, batch_size, learning_rate, test_dataset=validation_data)
+learning_rate = 3.0
+regulation_factor = 0.1
+# training_data = training_data[:500]
+net.SGD(training_data, num_epochs, batch_size, learning_rate, regulation_factor, test_dataset=validation_data)
