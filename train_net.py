cf62191b5d1ab68b817b8bc7c421e7879744d749
Tuan Nguyen <efranchisor@gmail.com>
2020-06-02 13:58:44 -0700

    train network with different methods of loss functions

---
+++ train_net.py
@@ -0,0 +1,16 @@
+import sys
+sys.path.append('./data')
+import quadratic_network
+import data_loader
+# Load datasets
+training_data, validation_data, test_data = data_loader.wrap_data()
+# Create and configure a network
+net = quadratic_network.DeepNet([784, 30, 10])
+# Train the network with hyper-parameters
+num_epochs = 30
+batch_size = 10
+learning_rate = 0.5
+regulation_factor = 0.1
+# training_data = training_data[:1000]
+loss_type = [ "MSE", "CE" ]
+net.SGD(training_data, num_epochs, batch_size, learning_rate, regulation_factor, loss_type[1], test_dataset=validation_data)
