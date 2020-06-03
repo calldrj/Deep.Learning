1813444707c4055a2b5b2d8770efe7b6cb6ed563
Tuan Nguyen <efranchisor@gmail.com>
2020-06-03 16:52:47 -0700

    revise training data saving module

--- train_net.py
+++ train_net.py
@@ -17,17 +17,17 @@ training_data, validation_data, test_data = data_loader.wrap_data()
 # Create and configure a network
 net = neuron_net.DeepNet([784, 30, 10])
 # Train the network with hyper-parameters
-num_epochs = 40
+num_epochs = 400
 batch_size = 10
 learning_rate = 0.5
-regulation_factor = 0.01
+regulation_factor = 0.1
 training_data = training_data[:1000]
-loss_type = [ "MSE", "CE" ]
-
+loss_types = [ "MSE", "CE" ]
+loss_type = loss_types[0]
 loss_train, loss_validation, \
 accuracy_train, accuracy_validation = net.SGD(training_data, num_epochs, batch_size, 
                                               learning_rate, regulation_factor, 
-                                              loss_type[1], test_dataset=validation_data)
+                                              loss_type, test_dataset=validation_data)
 # Create a data frame to save the loss and accuracy data
 df = pd.DataFrame({ "epoch":                np.arange(1, num_epochs + 1, 1),
                     "loss_train":           loss_train, 
@@ -35,4 +35,4 @@ df = pd.DataFrame({ "epoch":                np.arange(1, num_epochs + 1, 1),
                     "accuracy_train":       accuracy_train , 
                     "accuracy_validation":  accuracy_validation })
 # Write the data frame in local drive
-df.to_csv("{}neuron_net.csv".format(file_path), sep=',', index=False)
+df.to_csv("{}neuron_net_{}_{}.csv".format(file_path, num_epochs, loss_type), sep=',', index=False)
